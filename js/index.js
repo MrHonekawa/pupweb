e0063ba143e8a24043310fe05e6769b6bf7f9d21
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2023-12-10 23:26:35 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+var shibaAddr = null;
+var contract = null; 
 
+document.getElementById('connectWalletButton').onclick = async () => {
+  if (window.ethereum) {
+        await window.ethereum.send('eth_requestAccounts');
+        window.web3 = new Web3(window.ethereum);
+        var accounts = await web3.eth.getAccounts();
+        shibaAddr = accounts[0];
+        document.getElementById('connectWalletButton').textContent = "Connected!";
+        console.log(shibaAddr);
+  }
+}
